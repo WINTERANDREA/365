8d32466f1bea59ea5b8c23ee40de0a23bbc93505
winterandrea <andrecasero@gmail.com>
2020-11-04 09:28:41 +0100

    servizi

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//COUNTER
 $('.count').each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
@@ -9,3 +10,19 @@ $('.count').each(function () {
         }
     });
 });
+
+// COLLAPSIBLE 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.previousElementSibling;
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+    });
+}
